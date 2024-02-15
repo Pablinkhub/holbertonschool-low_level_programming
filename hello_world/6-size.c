fbefb1acacc1e1d8e8d39965c259b9d3b042910c
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-15 16:17:40 -0300

    sw

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,16 +5,16 @@
 * Return: "Algo que devuelve"
 */
 int main(void)
-{	
- char Char;
- int Int;
- long int LongInt;
- long long int LongLongInt;
- float Float;
-    printf("Size of a char: %zu byte(s)\n", sizeof(Char));
-    printf("Size of an int: %zu byte(s)\n", sizeof(Int));
-    printf("Size of a long int: %zu byte(s)\n", sizeof(LongInt));
-    printf("Size of a long long int: %zu byte(s)\n", sizeof(LongLongInt));
-    printf("Size of a float: %zu byte(s)\n", sizeof(Float));
-        return (0);
+{
+	char Char;
+	int Int;
+	long int LongInt;
+	long long int LongLongInt;
+	float Float;
+		printf("Size of a char: %zu byte(s)\n", sizeof(Char));
+		printf("Size of an int: %zu byte(s)\n", sizeof(Int));
+		printf("Size of a long int: %zu byte(s)\n", sizeof(LongInt));
+		printf("Size of a long long int: %zu byte(s)\n", sizeof(LongLongInt));
+		printf("Size of a float: %zu byte(s)\n", sizeof(Float));
+		return (0);
 }
