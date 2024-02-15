ce26d9871ff4e11f622e708cf777788e5eb281eb
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-15 17:10:54 -0300

    S

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,5 +16,23 @@ int main(void)
 		printf("Size of a long int: %zu byte(s)\n", sizeof(LongInt));
 		printf("Size of a long long int: %zu byte(s)\n", sizeof(LongLongInt));
 		printf("Size of a float: %zu byte(s)\n", sizeof(Float));
-		return (0);
+		 UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+#advanced
+Score: 0.00% (Checks completed: 0.00%)
+Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+
+You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
+Your program should return 1
+Your program should compile without any warnings when using the -Wall gcc option
+julien@ubuntu:~/c/$ gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -o quote 101-quote.c
+julien@ubuntu:~/c/$ ./quote
+and that piece of art is useful" - Dora Korpar, 2015-10-19
+julien@ubuntu:~/c/$ echo $?
+1
+julien@ubuntu:~/c/$ ./quote 2> q
+julien@ubuntu:~/c/$ cat q
+and that piece of art is useful" - Dora Korpar, 2015-10-19
+julien@ubuntu:~/c/$ grep printf < 101-quote.c
+julien@ubuntu:~/c/$ grep put < 101-quote.c
+julien@ubuntu:~/c/$ return (0);
 }
