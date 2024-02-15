67dd43879f078afb539933ef14b4ac512f630bc0
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-15 13:44:10 -0300

    dfe

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,6 +4,6 @@ int main(void)
 {
 /*Invoco la funcion "puts" para así lograr el output que se me pidió*/
 
-	puts ("Programming is like building a multilingual puzzle")
-	;return (0);
+	puts ('"Programming is like building a multilingual puzzle');
+	return (0);
 }	
