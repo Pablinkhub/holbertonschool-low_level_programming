01a9839fd38739922d90db774f5a7b13f3f6a44c
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-15 13:41:15 -0300

    ds

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
+/*Incluyo la libreria <studio.h> donde está incluida la función "puts"*/
 #include <stdio.h>
 int main(void)
 {
+/*Invoco la funcion "puts" para así lograr el output que se me pidió*/
+
 	puts ("Programming is like building a multilingual puzzle")
-	return 0;	
+	;return (0);
+}	
