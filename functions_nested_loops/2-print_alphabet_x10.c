e60726572a510048842f8b569d216c242a5edd54
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-19 22:41:57 -0300

    Hola Betty

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -10,11 +10,12 @@ void print_alphabet_x10(void)
 {
 	int a, count;
 
-	for (a = 97 ; a <= 122; a++)
+	for (count = 0; count < 10; count++)
 	{
-		 for (count = 0; count < 10; count++)
+		 for (a = 97 ; a <= 122; a++)
 		{
 			_putchar(a);
 		}
 	_putchar('\n');
+	}	
 }
