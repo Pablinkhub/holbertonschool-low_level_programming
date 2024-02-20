12a5ae51e5848a9ec1049a72ae4067031d285773
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-20 01:23:55 -0300

    Objetemos

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - a
+ *
+ *@c: Letter
+ *
+ * Return: always 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c < 122)
+		return (1);
+	return (0);
+}
