2fd96ca16a23d5190b293095af932b50bd86fcfd
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-20 14:45:32 -0300

    YES

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result.
+ *
+ *@b: integer
+ *
+ *@a: integer
+ *
+ * Return: integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
