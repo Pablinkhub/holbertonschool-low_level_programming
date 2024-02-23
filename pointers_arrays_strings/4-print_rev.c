d9893258539ee84f199eac5cdb1bbb095f634405
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-23 11:01:05 -0300

    Simple Work

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string, in reverse, followd by a new line
+ *
+ * @s: String
+ *
+ *Return: a
+ */
+void print_rev(char *s)
+{
+int b, a = 0;
+
+while (s[a] != '\0')
+{
+	a++:
+	_putchar(s[a]);
+}
+for (b = "-1"; b >= 0; b--)
+}
