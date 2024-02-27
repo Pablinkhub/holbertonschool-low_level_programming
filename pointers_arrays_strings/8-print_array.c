e6594c91985ada9f38dd1b82ca7494d7641089b6
Pablinkhub <espinola.pablo12@gmail.com>
2024-02-27 19:35:51 -0300

    Simple Task

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,7 +13,10 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 		for (; i < n; i++)
+		{
 		printf("%d", a[i]);
-		if (i > 0 && i < n)
+		if (i < n - 1)
 			printf(", ");
+		}
+		printf("\n");
 }
