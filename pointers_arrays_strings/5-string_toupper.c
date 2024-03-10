d69e14bebdeca50c8128b898a1266acc1cdee065
Pablinkhub <espinola.pablo12@gmail.com>
2024-03-10 18:34:23 -0300

    Changes to uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *string_toupper - changes all lowercase letters of a string to uppercase.
+ * @c: string.
+ * Return: @c in upper
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] -= 32;
+	return (c);
+}
