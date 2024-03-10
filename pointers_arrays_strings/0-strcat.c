bbea92e8c393a551b7297a56c64001a60c478cc6
Pablinkhub <espinola.pablo12@gmail.com>
2024-03-10 18:06:16 -0300

    Tarea

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,16 +10,14 @@
 */
 char *_strcat(char *dest, char *src)
 {
+	char *r = dest;
+
 	while (*dest != '\0')
 	{
 		dest++;
 	}
 	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (dest);
+	*dest++ = *src++;
+	*dest++ = '\0';
+	return (r);
 }
