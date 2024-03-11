5161c66139e787c76ed1ab3a7f5300cda53e8162
Pablinkhub <espinola.pablo12@gmail.com>
2024-03-10 21:08:39 -0300

    Locate Ocurrence in the string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strpbrk - locates the first occurrence in the string
+ * @s: string.
+ * @accept: char
+ *
+ * Return: first segment found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int k, j;
+
+	for (k = 0; s[k] != '\0'; k++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[k] == accept[k])
+			{
+				return (s + k);
+			}
+	}
+	return (0);
+}
